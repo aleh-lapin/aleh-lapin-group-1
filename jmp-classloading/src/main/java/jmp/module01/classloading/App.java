255a6703a7a93b89f317241161ba5c0687566be3
Pavel Khatsimtsau <PHotimtsev@gmail.com>
2014-10-07 09:23:04 +0300

    Fix problem with entering

--- jmp-classloading/src/main/java/jmp/module01/classloading/App.java
+++ jmp-classloading/src/main/java/jmp/module01/classloading/App.java
@@ -15,30 +15,25 @@ public class App
 	{	
 		LOG.info(CONSOLE_MESSAGE);
 		Scanner scanner = new Scanner(System.in);
-		String readString = scanner.nextLine();
-		while(readString !=null )
+		String readString = null;
+		while((readString = scanner.nextLine())  != null )
 		{
 			LOG.info(readString);
 			if(readString.equals(""))
 				LOG.info(CONSOLE_MESSAGE);
-			if(scanner.hasNextLine()) {
-				readString = scanner.nextLine();
-				try {
-					JarEntryClassLoader loader = new JarEntryClassLoader(readString);
-					Class<?> clazz = loader.loadClass("com.jmpClassloadin.xml.bind.Person");
-					Object person = clazz.newInstance();
-					LOG.info(" Loaded class " + person);
-				} catch (IOException e) {
-					fatal(e.getLocalizedMessage());
-				} catch (ClassNotFoundException e) {
-					fatal(e.getLocalizedMessage());
-				} catch (InstantiationException e) {
-					fatal(e.getLocalizedMessage());
-				} catch (IllegalAccessException e) {
-					fatal(e.getLocalizedMessage());
-				}
-			} else {
-				readString = null;
+			try {
+				JarEntryClassLoader loader = new JarEntryClassLoader(readString);
+				Class<?> clazz = loader.loadClass("com.jmpClassloadin.xml.bind.Person");
+				Object person = clazz.newInstance();
+				LOG.info(" Loaded class " + person);
+			} catch (IOException e) {
+				fatal(e.getLocalizedMessage());
+			} catch (ClassNotFoundException e) {
+				fatal(e.getLocalizedMessage());
+			} catch (InstantiationException e) {
+				fatal(e.getLocalizedMessage());
+			} catch (IllegalAccessException e) {
+				fatal(e.getLocalizedMessage());
 			}
 		}
 		if (scanner != null) {
