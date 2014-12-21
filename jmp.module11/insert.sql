41d2d75d0a8f3431e776ae6f50fccd4e065e8633
PavelKhatsimtsau <PHotimtsev@gmail.com>
2014-12-21 20:13:51 +0300

    SQL SCRIPT

---
+++ jmp.module11/insert.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO `exchanger` (`id`, `account_definition`) VALUES
+(1, '{"course":[{"value":1000,"curr":"US"},{"value":2001,"curr":"UK"},{"value":400,"curr":"EU"}],"basicCurrency":"RU","bankRef":"SBER-BANK"}');
+
+INSERT INTO `account` (`id`, `account_definition`) VALUES
+(1, '{"person":{"firstName":"Person1","surName":"Person surname1","lastName":"Person lastname1","birthDate":"2014-11-22 11:40:49","workPlace":["Work place of person1"],"genre":"M","nationality":"BE","address":{"country":"Contry1","city":"City1","district":"District1","telephones":{"telephones":[{"value":"125 566 545","telephoneType":"HOME"},{"value":"375 12 56 55","telephoneType":"MOBILE"},{"value":"12 56 54","telephoneType":"WORK"},{"value":"12556","telephoneType":"VO_IP"}]}},"id":0},"credit":{"value":2000,"curr":"RU"},"debit":{"value":2,"curr":"RU"},"ballance":{"value":2000,"curr":"RU"},"lastUpdatedDate":"2014-11-22 11:40:49","id":1}');
+
+INSERT INTO `account` (`id`, `account_definition`) VALUES
+(2, '{"person":{"firstName":"Person1","surName":"Person surname1","lastName":"Person lastname1","birthDate":"2014-11-22 11:40:49","workPlace":["Work place of person1"],"genre":"M","nationality":"BE","address":{"country":"Contry1","city":"City1","district":"District1","telephones":{"telephones":[{"value":"125 566 545","telephoneType":"HOME"},{"value":"375 12 56 55","telephoneType":"MOBILE"},{"value":"12 56 54","telephoneType":"WORK"},{"value":"12556","telephoneType":"VO_IP"}]}},"id":0},"credit":{"value":2000,"curr":"RU"},"debit":{"value":2,"curr":"RU"},"ballance":{"value":2000,"curr":"RU"},"lastUpdatedDate":"2014-11-22 11:40:49","id":1}');
+
+INSERT INTO `account` (`id`, `account_definition`) VALUES
+(3, '{"person":{"firstName":"1212","surName":"xcvxcv","lastName":"xfvxcv","birthDate":"2014-10-12 00:00:00","workPlace":["sdfsdf"],"genre":"W","nationality":"ru","address":{"country":"sdfsdsd","city":"sdf","district":"sdfsdf","telephones":{"telephones":[{"value":"1245","telephoneType":"HOME"},{"value":"454212","telephoneType":"MOBILE"},{"value":"1212","telephoneType":"WORK"},{"value":"1212","telephoneType":"VoIP"}]}},"id":0},"credit":{"value":12,"curr":"EU"},"debit":{"value":1212,"curr":"RU"},"ballance":{"value":0.0,"curr":"RU"},"lastUpdatedDate":"2014-12-21 13:19:09","id":0}');
+
